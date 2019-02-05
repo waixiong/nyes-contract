90f0327a01cfb4f0408e06bf0420a2e3a38c2b7e
waixiong <waixiong@siswa.um.edu.my>
2019-02-05 19:29:09 +0800

    Updates

---
+++ precache-manifest.1a2d5ebfaf8d41e4f28a7235d09796c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nyes-contract/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ba929b8477e4224c5e3b",
+    "url": "/nyes-contract/static/js/runtime~main.ba929b84.js"
+  },
+  {
+    "revision": "e35481669a806f13e618",
+    "url": "/nyes-contract/static/js/main.e3548166.chunk.js"
+  },
+  {
+    "revision": "27a8b92c58f2bdaa04a9",
+    "url": "/nyes-contract/static/js/1.27a8b92c.chunk.js"
+  },
+  {
+    "revision": "e35481669a806f13e618",
+    "url": "/nyes-contract/static/css/main.c5502d93.chunk.css"
+  },
+  {
+    "revision": "27a8b92c58f2bdaa04a9",
+    "url": "/nyes-contract/static/css/1.ef0b91d4.chunk.css"
+  },
+  {
+    "revision": "ed6a1523da87603eba7012c25f346981",
+    "url": "/nyes-contract/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/nyes-contract/precache-manifest.1a2d5ebfaf8d41e4f28a7235d09796c2.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/nyes-contract/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
